d848e1cf404c4b9f1c5acdbf231f089030aea4cb
Garygohjr <garygohjr@gmail.com>
2021-01-25 17:56:54 +0800

    Added sample stripe api route

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//router files
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var stripeRouter = require('./routes/stripe')
 
 var app = express();
 
@@ -14,7 +16,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/v1/stripe', stripeRouter);
 
 module.exports = app;
---
+++ routes/stripe.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* Stipe (something) call */
+router.get('/', function(req, res, next) {
+  /*some stripe api call code*/
+});
+
+module.exports = router;
